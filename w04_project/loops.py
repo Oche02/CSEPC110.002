a7e676f9e2eb124e8c85c02eaf316a7b194e4cd5
oche02 <augustineoche02.com>
2026-09-23 15:52:42 +0100

    shortened the code while still practicing loops with viriable

--- w04_project/loops.py
+++ w04_project/loops.py
@@ -1,18 +1,26 @@
-#collect user input
-payment = float(input("What is your payment amout? $"))
-penalty = 0
-
-#Your payment should not be less than 0
+payment = -1
 while payment < 0:
-        penalty = 1.50 
-        print("Sorry. Your payment can not be negative.")
         payment = float(input("What is your payment amout? $"))
 
-        print("...")
-        print("...")
-        print("...")
-        # this is where it returns to the loop
+print(f"Your payment amount is ${payment:.2f}")
 
 
-#expected result
-print(f"Your payment amount is ${payment:.2f}. Penalty is ${penalty:.2f}")
+
+# #collect user input
+# payment = float(input("What is your payment amout? $"))
+# penalty = 0
+
+# #Your payment should not be less than 0
+# while payment < 0:
+#         penalty = 1.50 
+#         print("Sorry. Your payment can not be negative.")
+#         payment = float(input("What is your payment amout? $"))
+
+#         print("...")
+#         print("...")
+#         print("...")
+#         # this is where it returns to the loop
+
+
+# #expected result
+# print(f"Your payment amount is ${payment:.2f}. Penalty is ${penalty:.2f}")
